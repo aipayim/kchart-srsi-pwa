911a1bc61d68f739eed408f1d172c78dd29e0d33
aipayim <aipayim@users.noreply.github.com>
2026-09-04 00:52:20 +0800

    反转标记改灰白底+原信号色字

--- src/version.generated.js
+++ src/version.generated.js
@@ -3,7 +3,7 @@
 // 执行 "npm run version:gen" 重新生成。
 export const APP_VERSION = "1.0.0";
 export const APP_TAG = "";
-export const APP_COMMIT = "3a1f3dc";
-export const APP_DESCRIBE = "3a1f3dc";
+export const APP_COMMIT = "4913a70";
+export const APP_DESCRIBE = "4913a70";
 export const APP_DIRTY = true;
-export const APP_BUILD_TIME = "2026-09-03T16:40:11.515Z";
+export const APP_BUILD_TIME = "2026-09-03T16:51:27.102Z";
